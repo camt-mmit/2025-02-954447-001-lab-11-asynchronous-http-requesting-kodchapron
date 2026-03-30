a40cd14bd3a5e2369fdb16ef931e066f8d561fbb
Kodchapron <kodchapron.tangw@gmail.com>
2026-03-30 20:17:43 +0700

    week-13-14

--- src/app/google/config.ts
+++ src/app/google/config.ts
@@ -9,5 +9,5 @@ export const googleOauthConfig: OauthClientConfiguration = {
   authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   redirectUrl: isDevMode()
     ? 'http://localhost:4200/google/authorization'
-    : 'https://camt-mmit.github.io/2025-02-954447-001-lab-11-asynchronous-http-requesting-mrpachara/google/authorization',
+    : 'https://camt-mmit.github.io/2025-02-954447-001-lab-11-asynchronous-http-requesting-kodchapron/google/authorization',
 };
